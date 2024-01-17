36fef0444fb553e66792e533f145e1362785ca17
Scott Kehr <scott.kehr@gmail.com>
2024-01-17 11:13:45 -0500

    Coming Soon page

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"m9EhxEPEL0NeznPlFVwncCUYQ2KBc55hGtgSGI9kaZY=\"\n}"
---
+++ src/components/preloader/preloader.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import React, { Component } from "react";
+// import "./Preloader.css";
+
+import styles from "../preloader/preloader.module.scss";
+
+class Preloader extends Component {
+    
+  preloader() {
+    let preload = document.querySelector(".preloader") as HTMLElement;
+    if (preload) {
+        setTimeout(() => {
+            preload.style.opacity = "0";
+            setTimeout(() => {
+                preload.style.display = "none";
+            }, 1000);
+        }, 3000);
+    }
+  }
+
+  componentDidMount() {
+    this.preloader();
+  }
+
+  render() {
+    return (
+      <div className={styles.preloader + " preloader"}>
+        <div className={styles.spinner_wrap}>
+          <div className={styles.spinner} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Preloader;
