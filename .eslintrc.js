1c041dd0c94a4a0e7c14e99c820dc10944e0b2bc
Scott Kehr <scott.kehr@gmail.com>
2024-02-11 23:58:50 -0500

    Configuring and adding unit tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,18 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   extends: ['eslint:recommended', 'next/core-web-vitals', 'prettier'],
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  env: {
+    'jest/globals': true,
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import XIcon from '@mui/icons-material/X';
-import Facebook from '@mui/icons-material/Facebook';
-import LinkedIn from '@mui/icons-material/LinkedIn';
-import Instagram from '@mui/icons-material/Instagram';
-
 
 export default function App() {
   return (
-    <div className="app">
-        <div className="logo"></div>
+    <div className='app'>
+      <div className='logo'></div>
     </div>
   );
 }
