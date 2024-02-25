6f01a8a1e7a2fb8e7b8f00eedfcef3d43c926961
Michael Hallman <1537945+hallmanm@users.noreply.github.com>
2024-02-24 21:26:44 -0500

    moved icons to images folder. unable to properly reference yet.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from 'next';
+import type { Viewport } from 'next';
 import { inter } from '@/app/ui/fonts';
 import './ui/styles/_global.scss';
 
 export const metadata: Metadata = {
   title: 'Four Loop Digital',
   description: 'Digital Consulting Services',
+  manifest: '/site.webmanifest',
+};
+
+export const viewport: Viewport = {
+  themeColor: '#ffffff',
 };
 
 export default function RootLayout({
@@ -17,24 +23,28 @@ export default function RootLayout({
       <link
         rel='apple-touch-icon'
         sizes='180x180'
-        href='/apple-touch-icon.png'
+        href='ui/images/apple-touch-icon.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='32x32'
-        href='/favicon-32x32.png'
+        href='ui/images/favicon-32x32.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='16x16'
-        href='/favicon-16x16.png'
+        href='ui/images/favicon-16x16.png'
       />
-      <link rel='manifest' href='/site.webmanifest' />
-      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+      {/* <link rel='manifest' href='/site.webmanifest' /> */}
+      <link
+        rel='mask-icon'
+        href='ui/images/safari-pinned-tab.svg'
+        color='#5bbad5'
+      />
       <meta name='msapplication-TileColor' content='#da532c' />
-      <meta name='theme-color' content='#ffffff'></meta>
+      {/* <meta name='theme-color' content='#ffffff'></meta> */}
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
