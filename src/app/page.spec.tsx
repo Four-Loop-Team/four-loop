fe7fffdfc7ac8539e641963cc276ca171664ab9b
Scott Kehr <scott.kehr@gmail.com>
2024-02-25 21:53:35 -0500

    Fixing config for jest to work

--- src/app/page.spec.tsx
+++ src/app/page.spec.tsx
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import renderer from 'react-test-renderer';
 import App from './page';
 
