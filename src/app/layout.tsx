769216aec57182fe7c840ada722e08741ce46177
Michael Hallman <1537945+hallmanm@users.noreply.github.com>
2024-02-25 20:51:46 -0500

    debugging image paths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-import type { Metadata } from 'next';
-import type { Viewport } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { inter } from '@/app/ui/fonts';
 import './ui/styles/_global.scss';
 
@@ -20,31 +19,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <link
-        rel='apple-touch-icon'
-        sizes='180x180'
-        href='ui/images/apple-touch-icon.png'
-      />
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='32x32'
-        href='ui/images/favicon-32x32.png'
-      />
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='16x16'
-        href='ui/images/favicon-16x16.png'
-      />
-      {/* <link rel='manifest' href='/site.webmanifest' /> */}
-      <link
-        rel='mask-icon'
-        href='ui/images/safari-pinned-tab.svg'
-        color='#5bbad5'
-      />
-      <meta name='msapplication-TileColor' content='#da532c' />
-      {/* <meta name='theme-color' content='#ffffff'></meta> */}
+      <head>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        {/* <link rel='manifest' href='/site.webmanifest' /> */}
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        {/* <meta name='theme-color' content='#ffffff'></meta> */}
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function App() {
   return (
     <div className='app'>
       <div className='logo'></div>
+      <Image
+        src='/android-chrome-192x192.png'
+        width={500}
+        height={500}
+        alt='icon 1'
+      />
+      <Image
+        src='/ui/images/android-chrome-512x512.png'
+        width={500}
+        height={500}
+        alt='icon 2'
+      />
     </div>
   );
 }
