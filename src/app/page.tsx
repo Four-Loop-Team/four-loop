740f800c8eb642e03555147869b7c65706a0b453
Scott Kehr <scott.kehr@gmail.com>
2025-06-24 18:28:40 -0400

    Adding logo back into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ export default function App() {
   return (
     <Container maxWidth='lg' sx={{ py: 8 }}>
       <Box textAlign='center'>
+        <Box 
+          className='logo' 
+          sx={{ 
+            mx: 'auto', 
+            mb: 4,
+            display: 'inline-block' 
+          }} 
+        />
         <Typography variant='h2' component='h1' gutterBottom>
           Welcome to Four Loop Digital
         </Typography>
